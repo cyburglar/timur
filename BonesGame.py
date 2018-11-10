306d37308b30667ebd1136fff75947bc2fb6f908
Advernet <timur.biljalov@gmail.com>
2018-11-10 16:00:47 +0200

    added 5 bones, successfully

--- BonesGame.py
+++ BonesGame.py
@@ -6,6 +6,14 @@ print (b)
 
 bone1 = Bone()
 bone2 = Bone()
+bone3 = Bone()
+bone4 = Bone()
+bone5 = Bone()
+
 
 print (bone1.throw())
 print (bone2.throw())
+print (bone3.throw())
+print (bone4.throw())
+print (bone5.throw())
+
