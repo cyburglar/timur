b26d407f77170c6063d60cb75e0c737c0d44f331
Advernet <timur.biljalov@gmail.com>
2018-11-19 18:56:45 +0200

    added 5 bones, successfully

--- BonesGame.py
+++ BonesGame.py
@@ -8,3 +8,26 @@ bones = [Bone(), Bone(), Bone(), Bone(), Bone()]
 
 for x in bones:
     print (x.throw())
+
+class Glass:
+    value = 0
+    def __init__(self, contains, throw):
+        self.contains = [Bone(), Bone(), Bone(), Bone(), Bone()]
+        self.throw = throw
+
+class Glass1:
+    value = 0
+    def __init__(self, contains, throw):
+        self.contains = [Bone(), Bone(), Bone(), Bone(), Bone()]
+        self.throw = throw
+
+class Glass2:
+    value = 0
+    def __init__(self, contains, throw):
+        self.contains = [Bone(), Bone(), Bone(), Bone(), Bone()]
+        self.throw = throw
+
+
+
+
+
