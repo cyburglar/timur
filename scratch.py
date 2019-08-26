38e2d7c8029f1292f7d9d76494cb2e2aead91c50
Advernet <timur.biljalov@gmail.com>
2019-08-26 17:22:02 +0300

    пара вопросов

--- scratch.py
+++ scratch.py
@@ -1,8 +1,8 @@
-x = int(input())
-if x <= 10:
-    print("good")
-else:
-    print("bad")
-
+numbers = [2, 3, 5, 2, 7, 9, 5]
+uniques = [] # почему написанно именно uniques?
+for number in numbers: #почему написано имеено number, мы ведь не определили ни как именно слово number
+    if number not in uniques:
+        uniques.append(number)
+print(uniques)
 
 
